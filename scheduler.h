2102c3302644c4986df04869015c9efcb2a6a0d8
Benji Olsen <benjaminalanolsen@gmail.com>
2019-04-23 16:43:48 +0800

    Added header guard

--- scheduler.h
+++ scheduler.h
@@ -1,8 +1,11 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <time.h>
+#IFNDEF SCHEDULER_H
+    #define SCHEDULER_H
+    #include <time.h>
+    #include <pthread.h>
+    #include <stdio.h>
+    #include <sys/types.h>
+    #include <unistd.h>
 
-struct cpuTask {
-} cpuTask;
+    struct cpuTask {
+    } cpuTask;
+#ENDIF
