1409afb7cbeef18778a0e68023469877ebc529f8
Benji Olsen <benjaminalanolsen@gmail.com>
2019-04-25 16:27:14 +0800

    Added arg checks

--- scheduler.c
+++ scheduler.c
@@ -11,9 +11,20 @@ int main(int argv,char** argc){
 }
 
 void start(int argv,char** argc){
+    if(strcmp(argc[1],"task_file\0")==0){
+            if(!((atoi(argc[2])<1)||(atoi(argc[2])>10))){
+                /* Functionality yet to be implmented */
+            }else{
+                fprintf(stderr,"Please run as ./scheduler task_file m\n");
+                fprintf(stderr,"Where m is the maximum size of the ready queue (1-10)\n");
+            }
+    }else{
+        fprintf(stderr,"Please run as ./scheduler task_file m\n");
+        fprintf(stderr,"Where m is the maximum size of the ready queue (1-10)\n");
+    }
 }
-
 void cpu(){
+
 }
 
 void task(){
