a0899e39322f8b8ce3e2820c18bc702813ece53f
Benji Olsen <benjaminalanolsen@gmail.com>
2019-04-25 14:38:19 +0800

    Made a start on the functions to start the scheduler

--- scheduler.c
+++ scheduler.c
@@ -1,10 +1,19 @@
 #include "scheduler.h"
 int main(int argv,char** argc){
+    if(argv==3){
+        start(argv,argc);
+    }
+    else{
+        fprintf(stderr,"Please run as ./scheduler task_file m\n");
+        fprintf(stderr,"Where m is the maximum size of the ready queue (1-10)\n");
+    }
     return 0;
 }
 
+void start(int argv,char** argc){
+}
+
 void cpu(){
-    
 }
 
 void task(){
