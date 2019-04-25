24ae88e50da32c7bed6524ef668e9e81bc2aa3ce
Benji Olsen <benjaminalanolsen@gmail.com>
2019-04-25 14:37:49 +0800

    Added some predeclerations and included the string library

--- scheduler.h
+++ scheduler.h
@@ -5,9 +5,17 @@
     #include <stdio.h>
     #include <stdlib.h>
     #include <unistd.h>
+    #include <string.h>
     #include "io.h"
     #include "linkedlist.h"
 
+    #define TRUE 0
+    #define FALSE !TRUE
     struct cpuTask {
+        int placeHolder;
     } cpuTask;
+
+    void start(int,char**);
+    void cpu();
+    void task();
 #endif
