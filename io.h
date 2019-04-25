5f238bb81d95e2fe135a21dbf62dee9067ab1428
Benji Olsen <benjaminalanolsen@gmail.com>
2019-04-25 16:26:45 +0800

    Added header comments

--- io.h
+++ io.h
@@ -1,7 +1,19 @@
+/*
+Created by: Benjamin Olsen
+Student Number: 19481681
+Purpose: To act as the header file for the io c file. Contains the all the
+         predeclerations for the functions used. Also all the includes needed
+         for the io.c file to function.
+*/
 #ifndef IO_H
+    /* Defines a header guard to avoid multiple includes */
     #define IO_H
+
     #include "linkedlist.h"
     #include "scheduler.h"
+
+    /* Defines the buffer used for fgets when reading from the file */
     #define BUFFER_SIZE 1024
-    int readlines(char* filename);
+    /* Reads in the lines from the specified file, into a linked list */
+    int readlines(LinkedList* list,char* filename);
 #endif
