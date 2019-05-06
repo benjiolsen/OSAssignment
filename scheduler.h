1284d87e72bcad772fb8ba1d69ba5b287214f53f
Benji Olsen <benjaminalanolsen@gmail.com>
2019-05-06 14:04:10 +0800

    Comments and some edits to structs

--- scheduler.h
+++ scheduler.h
@@ -25,19 +25,33 @@ Purpose: To act as the header file for the scheduler c file. Contains the
     #define NUM_THREADS 4
 
     /* Defines the struct cpuTask, so that it can be used to store all the
-       information relating to the arrival, waiting, burst time, etc. */
+       information relating to the arrival, service time etc. */
     typedef struct{
-        int placeHolder;
+        time_t arrive;
+        time_t service;
     }cpuTask;
 
+    /* Defines the struct that contains all the variables to be shared
+       throughout the threads*/
     typedef struct{
+        /* The queue of items */
         LinkedList* ready_queue;
+        /* The id of each thread in an array */
         pthread_t thread_id[NUM_THREADS];
+        /* The mutex and condition variables for locking/waiting/signalling */
         pthread_mutex_t mutex;
         pthread_cond_t full;
         pthread_cond_t empty;
+        /* The total turnaround and waiting time
+           doubles are used as that is what timediff return type is*/
+        double total_turnaround_time;
+        double total_waiting_time;
+        /* The max size of the ready queue */
         int max;
+        /* The flag variable to exit loop conditions */
         int flag;
+        /* The number of tasks completed by each cpu */
+        int num_tasks[3];
     }shared;
 
     /* Acts to start the program by ensuring the user entered the args correctly
